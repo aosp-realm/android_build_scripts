288777b3154d878452201e95bbbb734be51fa6a3
dogpoopy <dogpoopy.git@gmail.com>
2024-11-22 19:48:17 +0800

    Create crave-derp_apollo.sh

---
+++ crave-derp_apollo.sh
@@ -0,0 +1,14 @@
+rm -rf .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-derp15 .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' ; \
+export BUILD_USERNAME=dogpoopy ; \
+export BUILD_HOSTNAME=android-build ; \
+export TZ=Asia/Manila ; \
+export DISABLE_STUB_VALIDATION=true ; \
+cd vendor/derp && curl -s https://raw.githubusercontent.com/aosp-realm/ota-stuff/refs/heads/main/0001-tools-Allow-json-to-be-generated-in-all-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
+source build/envsetup.sh && \
+lunch derp_apollo-user && \
+make installclean ; \
+mka derp
