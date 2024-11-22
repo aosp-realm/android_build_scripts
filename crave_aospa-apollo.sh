5e3cef26e576f5cf09ec6480050fd620d4658ed5
dogpoopy <dogpoopy.git@gmail.com>
2024-11-22 19:45:11 +0800

    Update crave_aospa-apollo.sh

--- crave_aospa-apollo.sh
+++ crave_aospa-apollo.sh
@@ -3,5 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b vau
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-vauxite .repo/local_manifests && \
 /opt/crave/resync.sh && \
 repo forall -c 'git lfs pull' ; \
+export BUILD_USERNAME=dogpoopy ; \
+export BUILD_HOSTNAME=android-build ; \
 export TZ=Asia/Manila ; \
 ./rom-build.sh apollo
