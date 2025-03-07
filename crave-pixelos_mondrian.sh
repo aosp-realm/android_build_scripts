99046ec14579692ba9920193b95cf00a90e78bd2
dogpoopy <dogpoopy.git@gmail.com>
2025-03-07 13:32:33 +0800

    Update crave-pixelos_mondrian.sh

--- crave-pixelos_mondrian.sh
+++ crave-pixelos_mondrian.sh
@@ -1,1 +1,1 @@
-source build/envsetup.sh && lunch aosp_mondrian-ap3a-userdebug && make installclean && mka bacon
+cd device/xiaomi/mondrian && git pull && cd - && cd vendor/xiaomi/mondrian && git pull && cd - && source build/envsetup.sh && lunch aosp_mondrian-ap3a-userdebug && make installclean && mka bacon
