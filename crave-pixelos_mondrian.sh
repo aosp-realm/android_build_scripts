713376bedc8f1a44aa110922a5dda4b9e447523b
dogpoopy <dogpoopy.git@gmail.com>
2025-12-11 22:23:32 +0800

    Update crave-pixelos_mondrian.sh

--- crave-pixelos_mondrian.sh
+++ crave-pixelos_mondrian.sh
@@ -1,2 +1,8 @@
-/opt/crave/resync.sh && \
-cd device/xiaomi/mondrian && git pull && cd - && cd vendor/xiaomi/mondrian && git pull && cd - && source build/envsetup.sh && lunch aosp_mondrian-ap3a-userdebug && make installclean && mka bacon
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/aosp-realm/android_build_manifest.git -b mondrian-pos .repo/local_manifests
+/opt/crave/resync.sh
+repo forall -c 'git lfs pull'
+source build/envsetup.sh
+lunch aosp_mondrian-ap3a-userdebug
+make installclean
+mka bacon
